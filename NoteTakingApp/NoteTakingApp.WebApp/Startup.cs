549c42055b44ec875d87be2f5243a6f7fa8b45f7
Sean Fitzmaurice <seanfitzmaurice27@gmail.com>
2021-09-01 15:54:54 -0400

    change

--- NoteTakingApp/NoteTakingApp.WebApp/Startup.cs
+++ NoteTakingApp/NoteTakingApp.WebApp/Startup.cs
@@ -59,7 +59,7 @@ namespace NoteTakingApp.WebApp
 
             services.AddControllersWithViews();
         }
-
+        //change here
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
